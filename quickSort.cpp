4b509120f01aa65fab7418de6cd134281326b4d2
haotianmichael <2398419426@buaa.edu.cn>
2019-10-23 16:53:43 +0800

    理解快速排序的边界条件和分治思想
    Signed-off-by: haotianmichael <2398419426@buaa.edu.cn>

---
+++ quickSort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+const int N = 1e6 + 10;
+int n;
+int q[N];
+
+
+void quickSort(int q[], int l, int r) {
+
+    if(l >= r)return;
+    int x = q[l], i = l - 1, j = r + 1;
+    while(i < j) {
+    
+        do i++; while(q[i]<x); 
+        do j--; while(q[j]>x); 
+        if(i<j) swap(q[i], q[j]); 
+    
+    }
+    quickSort(q, l, j);
+    quickSort(q, j + 1, r);
+
+}
+
+
+int 
+main(void) {
+
+    scanf("%d", &n); 
+    for(int i = 0; i < n; i++)  scanf("%d", &q[i]);
+    
+    quickSort(q, 0, n-1);
+
+    for(int j = 0; j < n; j++) printf("%d ", q[j]);
+
+    return 0;
+
+}
