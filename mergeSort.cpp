2bbf52b3c72a886154168934760b9ae0d5e1d29b
haotianmichael <2398419426@buaa.edu.cn>
2019-10-24 10:46:06 +0800

    将merge排序改成从大到小
    Signed-off-by: haotianmichael <2398419426@buaa.edu.cn>

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -6,26 +6,28 @@ const int N = 1e6 + 10;
 int q[N], tmp[N];
 int n;
 
-
-
-void merge_Sort(int q[], int l, int r) {
-
-    if(l >= r) return;
+void merge_sort(int q[], int l, int r)
+{
+    if (l >= r) return;
 
     int mid = (l + r) >> 1;
-    merge_Sort(q, l, mid), merge_Sort(q, mid + 1, r);
-    
+    merge_sort(q, l, mid);
+    merge_sort(q, mid + 1, r);
+
     int k = 0, i = l, j = mid + 1;
-    while( i <= mid && j <= r) 
-        if(q[i] <= q[j])  tmp[k ++] = q[i ++];
-        else tmp[k ++] = q[j ++];
+    while (i <= mid && j <= r)
+        if (q[i] > q[j]) {
+            tmp[k ++ ] = q[i ++ ];
+        }else {
+            tmp[k ++ ] = q[j ++ ];
+        }
+    while (i <= mid) {
+        tmp[k ++ ] = q[i ++ ];
+    }
+    while (j <= r) tmp[k ++ ] = q[j ++ ];
 
-    while(i <= mid)  tmp[k ++] = q[i ++];
-    while(j <= r) tmp[k ++] =  q[j ++];
-
-    for(i = l, j = 0; i <= r; i++, j++) q[i] = tmp[j];
+    for (i = l, j = 0; i <= r; i ++, j ++ ) q[i] = tmp[j];
 }
-
 
 int 
 main(void) {
@@ -33,7 +35,7 @@ main(void) {
     scanf("%d", &n);
     for(int i = 0; i < n; i ++ )  scanf("%d", &q[i]);
 
-    merge_Sort(q, 0, n - 1);
+    merge_sort(q, 0, n - 1);
 
     for(int j = 0; j < n; j ++ ) printf("%d ", q[j]);
 
