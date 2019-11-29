988d74072ee9707f4d583fcf0d0703605ce0a290
haotianmichael <2398419426@buaa.edu.cn>
2019-11-29 21:41:39 +0800

    今天的focus可以  保持
    Signed-off-by: haotianmichael <2398419426@buaa.edu.cn>

---
+++ chp4/exgcd.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+typedef  long long LL;
+
+int gcd(int a, int b) {
+    return (b ? gcd(b, a % b) : a); 
+}
+
+int exgcd(int a, int b, int &x, int &y) {
+    if(!b) {
+        x = 1, y = 0;
+        return a; 
+    }
+    int d = exgcd(b, a % b, y, x);
+    y -= a / b * x;
+    return d;        
+}
+
+//求线性同余方程
+int exgcdII(int a, int b, int &x, int &y) {
+    if(!b)
+    {
+        x = 1, y = 0;
+        return a; 
+    }
+
+    int d = exgcdII(b, a % b, y, x);
+    y -= a / b * x;
+    return d;
+}
+
+
+int test_exgcdII(void) {
+
+    int n;
+    cin >> n;
+    while(n --){
+        int a, b, m, x, y;
+        cin >> a >> b >> m;
+
+        int d = exgcdII(a, m, x, y);
+        if(b%d) cout << "impossible" << endl;
+        else cout << (LL)a*(b/d)%m << endl;
+
+    return 0;
+}
+
+int 
+main(void) {
+
+    int n;
+    cin >> n;
+    while(n --) {
+        int a, b, x, y;
+        cin >> a >> b;
+        exgcd(a, b, x, y); 
+        cout << x << ' ' << y << endl;
+    }
+
+    return 0;
+}
---
+++ chp4/gauss.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std; const int N = 110;
+const double eps = 1e-6;
+double a[N][N];
+int n;
+
+/*
+    高斯消元法 
+*/
+int gauss() {
+
+    int r, c;
+    for(r = 0, c = 0; c < n; c ++) {
+        //c列  最大值
+        int t = r;
+        for(int i = r; i < n + 1; i ++) 
+            if(fabs(a[i][c]) > fabs(a[t][c]))  t = i;
+
+        if(fabs(a[t][c]) < eps) continue;
+
+        //换到第一行
+        for(int i = c; i < n + 1; i++) swap(a[r][i], a[t][i]);
+        //第一个值赋值1
+        for(int i = n; i >= c; i --) a[r][i] /= a[r][c];
+
+        //清零
+        for(int i = r + 1;  i < n; i ++)
+            if(fabs(a[i][c]) > eps)
+                for(int j = n; j >= c; j --) 
+                    a[i][j] -= a[r][j] * a[i][c];
+        r ++; 
+    }
+
+    if(r < n) {
+        for(int i = r; i < n; i ++) 
+            if(fabs(a[i][n])) return 2;
+        return 1;
+    }
+
+    for(int i = n - 1; i >= 0; i --)
+        for(int j = i + 1; j < n; j ++)
+            a[i][n] -= a[j][n] * a[i][j];
+
+    return 0;
+}
+
+
+int 
+main(void) {
+
+    cin >> n;
+    for(int i = 0; i < n; i++) 
+        for(int j = 0; j <  n + 1; j ++)
+            cin >> a[i][j];
+
+    int t = gauss();
+
+    if(t == 0) {
+        for(int i = 0;  i < n; i ++) printf("%.2lf\n", a[i][n]); 
+    }else if(t == 1) cout << "Infinite group solutions" << endl;
+    else cout << "No solution" << endl;
+
+    return 0;
+}
