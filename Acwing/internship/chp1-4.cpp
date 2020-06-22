8599cf0f21ed91bc48bf1efec748704b340a9883
haotianmichael <2398419426@buaa.edu.cn>
2020-06-22 10:14:17 +0800

    动态规划
    Signed-off-by: haotianmichael <2398419426@buaa.edu.cn>

---
+++ Acwing/internship/chp1-4.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+const int N = 110, M = 11;
+int n, m;
+int f[N][M];
+
+/*动态规划
+  f[i][j]存的是次数
+ */
+int DP1(void)
+{
+    while(cin >> n >> m){
+
+        for(int i = 1; i <= n; i ++) f[i][1] = i;
+        for(int i = 1; i <= m; i ++) f[1][i] = 1;
+
+        for(int i = 2; i <= n; i++)
+            for(int j = 2; j <= m; j ++) {
+                f[i][j] = f[i][j-1];
+                for(int k = 1; k <= i; k ++) 
+                    f[i][j] = min(f[i][j], max(f[k-1][j-1], f[i-k][j]) + 1);
+            }
+
+        cout << f[n][m] << endl; 
+    } 
+    return 0;
+}
+
+
+
+int DP2(void) {
+
+    while(cin >> n >> m){
+        for(int i = 1; i <= n; i ++)  {
+            for(int j = 1; j <= m; j ++) f[i][j] = f[i-1][j] + f[i-1][j-1] + 1;
+            if(f[i][m] >= n) {
+                printf("%d\n", i);          
+                break;
+            }
+        }
+    }
+    return 0;
+}
+
+
+int main(void)
+{
+    DP1();
+
+    return 0;
+}
