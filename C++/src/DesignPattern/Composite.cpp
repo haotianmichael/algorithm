f7fda2888c6760cbe989847e774ef8c8717e0866
haotianmichael <2398419426@buaa.edu.cn>
2020-09-08 08:04:41 +0800

    DesighPattern: Iterator Composite
    Signed-off-by: haotianmichael <2398419426@buaa.edu.cn>

--- C++/src/DesignPattern/Composite.cpp
+++ C++/src/DesignPattern/Composite.cpp
@@ -5,8 +5,6 @@
 
 using namespace std;
 
-
-
 /*
 动机: 
    在软件某些情况下，客户代码过多地依赖于对象容器复杂的内部实现结构，对象容器内部实现结构(而非
@@ -17,10 +15,10 @@ using namespace std;
 */
 class Component{
     public:
-        virtual void Process() = 0;
+        virtual void process() = 0;
         virtual ~Component();
 };
-
+//树节点
 class Composite : public Component{
     string name;
     list<Component*> elements;
@@ -36,14 +34,55 @@ class Composite : public Component{
     }
 
     void process() {
-    
-    
+        
+       //1. process current node
+
+       //2. process leaf node 
+
+        for(auto &e : elements){
+            e->process(); //多态调用
+        } 
     }
+};
+//叶子节点
+class Leaf : public Component{
+    string name;
+    public:
+    Leaf(string s) : name(s){} 
 
-
-
+    void process() {
+        //process current node 
+    }
 };
 
+static void Invoke(Component& c) {
+    c.process();  //多态调用
+} 
+
+int main(void)
+{
+    Composite root("root");     
+    Composite treeNode1("treeNode1");
+    Composite treeNode2("treeNode2");
+    Composite treeNode3("treeNode3");
+    Composite treeNode4("treeNode4");
+    Leaf leat1("left1");
+    Leaf leat2("left2");
+
+
+    root.add(&treeNode1);
+    treeNode1.add(&treeNode2);
+    treeNode2.add(&leat1);
+
+
+    root.add(&treeNode3);
+    treeNode3.add(&treeNode4);
+    treeNode4.add(&leat2);
+
+    Invoke(root);
+
+    return 0;
+}
 /*
 定义: 
     将对象组合成树形结构以表示“部分-整体”的层次结构。
@@ -55,7 +94,12 @@ class Composite : public Component{
 
 /*
 总结: 
- 
+   Composite模式采用树形结构来实现普遍存在的对象容器，从而将“一对多”的关系转换为“一对一”的关系
+   使得客户代码可以一致的（复用）处理对象和对象容器，无需关心处理的是单个对象，还是组合的对象容器。 
 
+   将“客户代码与复杂的对象容器结构”解耦是Composite的核心思想，解耦之后，客户代码将与纯粹的抽象接口
+   ——而非对象容器的内部实现结构——发生依赖，从而更能“应对变化”。
  
+   Composite模式在具体实现中，可以让父对象中的子对象反向追溯；如果父对象有频繁的额遍历需求，可使用缓存
+   技巧来改善效率。
 */
---
+++ C++/src/DesignPattern/Iterator.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <iterator>
+
+
+using namespace std;
+
+/*
+动机: 
+   在软件构建过程中，集合对象内部结构常常变化各异。但对于这些集合对象，我们希望在不暴露其内部结构
+   的同时，可以让外部客户代码透明的访问其中包含的元素；同时这种“透明遍历”也为“同一种算法在多种
+   集合对象上进行操作”提供了可能。
+
+   使用面向对象技术将这种遍历机制抽象为“迭代器对象”为“应对变化中的集合对象”提供了一种优雅的方式。 
+
+   模板:编译时多态
+   虚函数:运行时多态
+   面向对象的迭代器模式已经过时(性能损耗严重) ，使用泛型编程中的迭代器思想
+*/
+template<typename  T>
+class Iterator{
+    public:
+        virtual void first() = 0;
+        virtual void next() = 0;
+        virtual bool isDone() = 0;
+        virtual T& current() = 0;
+};
+
+template<typename  T>
+class MyCollection{
+    public:
+        Iterator<T> GetIterator() {
+            //... 
+        }
+
+};
+
+template<typename T>
+class CollectionIterator : public Iterator<T> {
+        MyCollection<T> mc;
+    public:
+        CollectionIterator(const MyCollection<T>& c) : mc(c) {}
+        virtual void first() {
+        }
+        virtual void next(){
+        } 
+        virtual bool isDone() const {
+        }
+        virtual T& current() {
+        }
+};
+
+
+void MyAlgorithm() {
+
+    MyCollection<int> mc;
+    Iterator<int> iter = mc.GetIterator();
+
+    for(iter.first(); !iter.isDone(); iter.next()) {
+        cout << iter.current() << endl; 
+    }
+}
+/*
+定义: 
+    提供一种方式顺序访问一个聚合对象中的各个元素，而又不暴露(稳定)该对象的内部表示。
+*/
+
+
+
+/*
+总结: 
+    迭代抽象: 访问一个聚合对象的内容而无需暴露它的内部表示
+
+    迭代多态：为遍历不同的集合结构提供一个统一个接口，从而支持同样的算法在不同的集合结构上进行操作。
+
+    迭代器的健壮性考虑: 遍历的同时更改迭代器所在的集合结构，会导致问题。
+*/
