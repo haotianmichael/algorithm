5ffecdea57797ef4b456b21b7951a92c48df9829
haotianmichael <2398419426@buaa.edu.cn>
2020-09-12 15:44:27 +0800

    Cucorrency: add thread_pool.cpp
    Signed-off-by: haotianmichael <2398419426@buaa.edu.cn>

---
+++ C++/src/C++ConCurrency/thread_pool.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <mutex>
+#include <thread>
+#include <future>
+#include <atomic>
+
+
+using namespace std;
+
+
+/*
+ 
+    chp11. 线程池谈，数量谈，总结
+    1. 虚假唤醒。 atomic
+
+
+    2. 线程池
+        2.1 场景设想
+
+
+        2.2 实现方式
+
+
+    3. 线程创建数量
+
+
+    4. 多线程总结    
+ 
+ 
+*/
+
+int main(void)
+{
+    
+    return 0;
+}
