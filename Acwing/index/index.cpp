f72fb9dc12a889c7a034221279a19ed6ef5bf1ac
haotianmichael <2398419426@buaa.edu.cn>
2020-10-12 09:19:43 +0800

    update: highprecision_add
    Signed-off-by: haotianmichael <2398419426@buaa.edu.cn>

--- Acwing/index/index.cpp
+++ Acwing/index/index.cpp
@@ -1,107 +1,78 @@
-#include <iostream>
 #include <algorithm>
 #include <cstring>
+#include <iostream>
+#include <vector>
+#include <string>
+
 using namespace std;
-
-/*
- 
-   快速排序
-   归并排序
-   二分法
-*/
-
 const int N = 100010;
-int a[N], n;
-int tmp[N];
+int a[N];
 
-void quickSort(int a[], int l, int r) {
-
-    if(l >= r) return;
-
-    int x = a[(l + r) >> 1], i = l - 1, j = r + 1;
-
-    while(i < j) {
-        do i ++; while(a[i] < x) ;
-        do j --; while(a[j] > x);
-        if(i < j) swap(a[i] , a[j]); 
-    }
-
-    quickSort(a, l, j);
-    quickSort(a, j + 1, r);
-}
-
-void mergeSort(int a[], int l, int r) {
-
-    if(l >= r) return;
-
-    int mid = (l + r) >> 1; 
-    mergeSort(a, l, mid);
-    mergeSort(a, mid + 1, r);
-
-    int i = l, j = mid + 1;
-    int k = 0;
-    while(i <= mid && j <= r) {
-        if(a[i] < a[j])  tmp[k++] = a[i++];
-        else tmp[k++] = a[j++]; 
-    }
-
-    while(i <= mid) tmp[k++] = a[i++];
-    while(j <= r) tmp[k++] = a[j++]; 
-
-    for(int i = l, j = 0; i <= r; i ++, j ++) a[i] = tmp[j]; 
-}
-
-
-/*
- 
-     给出一列数，判断其上下界
- 
-*/
-int x;
 void bSearch() {
-    int lhs, rhs;
+
+    int x, n;
     cin >> n >> x;
-    for(int i = 0; i < n; i ++) cin >> a[i]; 
-    //左边界
+    int lhs, rhs;
+
+        //左边界
     lhs = 0, rhs = n - 1;
     while(lhs < rhs) {
-        int mid = (lhs + rhs) >> 1; 
-        if(a[mid] == x) rhs = mid;
-        else lhs = mid  + 1;
+        int mid  = (lhs + rhs) >> 1; 
+        if(a[mid] >= x) rhs = mid;
+        else lhs = mid + 1; 
     }
- 
 
-    //右边界   
     if(lhs < rhs) {
-        cout << "-1, -1" << endl;
+        cout << "-1, -1" << endl; 
     }else {
+        //右边界
         cout << lhs << " ";
-        lhs = 0, rhs = n - 1; 
+        lhs  = 0, rhs =  n - 1;
         while(lhs < rhs) {
-            int mid  = (lhs + rhs + 1) >> 1; 
-            if(a[mid] == x) lhs = mid;
-            else rhs = mid - 1;
-        } 
-        cout << lhs << endl;
+            int mid = (lhs  + rhs + 1) >> 1; 
+            if(a[mid] <= x)  lhs = mid;
+            else rhs = mid - 1; 
+        }
+        cout << rhs << endl; 
     }
 
+
 }
 
+//高精度add
+vector<int> add(vector<int> &A, vector<int> &B) {
+    int res = 0;
+    vector<int> C;
+    unsigned int i = 0;
+    for(;i < A.size() || i < B.size() || res; i ++){
+        if(i < A.size())  res += A[i];
+        if(i < B.size())  res += B[i];
+        C.push_back(res % 10);
+        res /= 10;
+    }
 
-
-int mysort(void)
-{
-    cin >> n;
-    for(int i = 0; i < n; i ++)  cin  >> a[i]; 
-    //quickSort(a, 0, n - 1);
-    mergeSort(a, 0, n - 1);
-    for(int i = 0; i < n; i ++) cout << a[i] <<  ' '; 
-    return 0;
+    return C;
 }
+
+vector<int> sub(vector<int> &A, vector<int> &B) {
+    int res = 0;
+    vector<int> C;
+
+    return C;
+}
+
 
 
 int main(void)
 {
-    bSearch();
+    string a,b;
+    cin >> a >> b;
+    vector<int> A, B;
+    for(int i = a.size() - 1; i >= 0; i --)   A.push_back(a[i] - '0');
+    for(int i = b.size() - 1; i >= 0; i --)   B.push_back(b[i] - '0'); 
+    //auto C = add(A, B);
+    auto C = sub(A, B);
+    for(int i = C.size() - 1; i >= 0; i --) cout << C[i];  
+    puts("");
     return 0;
 }
