6c8220be2444e336789ff8b0e964785a09ba462d
haotianmichael <2398419426@buaa.edu.cn>
2020-11-09 09:24:47 +0800

    add RTTI
    Signed-off-by: haotianmichael <2398419426@buaa.edu.cn>

--- C++/src/InsideTheC++ObjectModel/constructor.cpp
+++ C++/src/InsideTheC++ObjectModel/constructor.cpp
@@ -8,3 +8,33 @@ using namespace std;
  
  
 */
+class A{
+    virtual void fun(){
+        ;
+    }
+};
+class B : public A{};
+class C : public B{};
+class D : public B{};
+class S {
+    virtual ~S(){}
+};
+
+
+int main(void)
+{
+/*    A* pa = new C;*/
+    //B *pb = dynamic_cast<B*>(pa);
+
+    B* pb = new B;
+    S* ps = new S;
+    C *pc = dynamic_cast<C*>(ps);
+
+/*    A *pa = new D;*/
+    //A *pb = dynamic_cast<B*>(pa);
+
+    return 0;
+}
+
+
+
